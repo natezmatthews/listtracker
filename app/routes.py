77d0efeddde7999f55300f35edda2d3eb1243ff9
Nate Matthews <natezmatthews@gmail.com>
2018-01-15 21:26:45 -0500

    Using session to get data between screens

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,22 @@
-from flask import render_template
+from flask import render_template, session, redirect, url_for
 from app import app
 from app.Classes import ComparisonForm, RisutoForm, Risuto
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    try:
+        text = session['text']
+    except:
+        text = 'Default'
     lists1 = ['X','Y','Z']
     lists2 = ['B','C','D','E']
     form = ComparisonForm()
     form.risuto1.choices = [(u,u) for u in lists1] # Choices must be set after initiation of form
     form.risuto2.choices = [(u,u) for u in lists2] # Choices must be set after initiation of form
     if form.validate_on_submit():
-        print('Valid')
-        print(form.left.data)
-    else:
-        print('inValid')
-        print(form.left.data)
-    return render_template('index.html',ulists=lists1,form=form)
+        pass
+    return render_template('index.html',text=text,form=form)
 
 @app.route('/create',methods=['GET','POST'])
 def create():
@@ -24,7 +24,9 @@ def create():
     if form.validate_on_submit():
         print('Valid')
         print(form.risuto.data)
+        session['text'] = form.risuto.data
+        return redirect(url_for('index'))
     else:
-        print('inValid')
+        print ('inValid')
         print(form.risuto.data)
     return render_template('create.html',form=form)
