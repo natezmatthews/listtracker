2e3a61d7ae2f626a429d6925ee6db0f445ef2299
Nate Matthews <natezmatthews@gmail.com>
2018-01-16 20:49:58 -0500

    Create form now interacts with front page dropdowns

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,45 @@
 from flask import render_template, session, redirect, url_for
 from app import app
 from app.Classes import ComparisonForm, RisutoForm, Risuto
-import sqlite3
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    if 'risutos' in session:
-        risutos = [x.risutotext for x in session['risutos']]
-    else:
-        risutos = ['Nothing yet']
-    lists1 = ['X','Y','Z']
-    lists2 = ['B','C','D','E']
+    print(session)
     form = ComparisonForm()
-    form.risuto1.choices = [(u,u) for u in lists1] # Choices must be set after initiation of form
-    form.risuto2.choices = [(u,u) for u in lists2] # Choices must be set after initiation of form
+    # Choices must be set after initiation of form
+    if 'risutos' in session:
+        risutos = [Risuto.fromdict(r) for r in session['risutos']]
+        choices = [(r,r.name) for r in risutos]
+    else:
+        choices = [(None,'Nothing yet')]
+
+    form.risuto1.choices = choices
+    form.risuto2.choices = choices
+    
     if form.validate_on_submit():
         pass
-    return render_template('index.html',risutos=risutos,form=form)
+    return render_template('index.html',form=form)
 
 @app.route('/create',methods=['GET','POST'])
 def create():
     form = RisutoForm()
+    print(session)
     if form.validate_on_submit():
+        print(form.risuto.data)
         risuto = Risuto()
-        risuto.name = 'Name' + form.risuto.data[:5]
-        risuto.description = 'Desc' + form.risuto.data[:5]
+        risuto.name = 'Name: ' + form.risuto.data[:5]
+        risuto.description = 'Desc: ' + form.risuto.data[:5]
         risuto.risutotext = form.risuto.data
+
+        risutodict = risuto.todict()
         if 'risutos' in session:
-            session['risutos'].append(risuto)
+            risutos = session['risutos']
+            risutos.append(risutodict)
+            session['risutos'] = risutos
         else:
-            session['risutos'] = [risuto]
+            session['risutos'] = [risutodict]
+        
         return redirect(url_for('index'))
+    
     return render_template('create.html',form=form)
