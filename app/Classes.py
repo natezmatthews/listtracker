7d45a5a10a2149845cf486f0dadad5579afc337d
Nate Matthews <natezmatthews@gmail.com>
2018-01-16 20:11:14 -0500

    Added to and from dict methods to Risuto

--- app/Classes.py
+++ app/Classes.py
@@ -9,11 +9,12 @@ class Risuto():
         self._separators = [",","\n"] # Default separators
         self._risutoset = set()
     
-    def _strvalidation(self,value,maxlen=None):
-        assert isinstance(value, str), "This field must be a string."
-        assert value, "This field may not be an empty string."
+    def _strvalidation(self,value,field,maxlen=None):
+        assert isinstance(value, str), "The {} must be a string.".format(field)
+        assert value, "The {} may not be an empty string.".format(field)
         if maxlen:
-            assert maxlen >= len(value), "This string must be {} or fewer characters.".format(maxlen)
+            assert maxlen >= len(value), \
+                   "The {} must be {} or fewer characters.".format(field,maxlen)
     
     @property
     def risutotext(self):
@@ -21,7 +22,7 @@ class Risuto():
     
     @risutotext.setter
     def risutotext(self,value):
-        self._strvalidation(value)
+        self._strvalidation(value,field='text')
         self._risutotext = value
         self._risutoset_setter()
         
@@ -31,7 +32,7 @@ class Risuto():
         del self._risutoset
 
     def addseparator(self,sep):
-        self._strvalidation(sep)
+        self._strvalidation(sep,field='separator')
         self._separators.append(sep)
 
     def removeseparator(self,sep):
@@ -52,12 +53,14 @@ class Risuto():
             for elem in oldrisuto:
                 newrisuto.extend(elem.split(sep))
         
-        # The filter gets rid of the empty strings split creates when there are two or more delimiters in a row
+        # The filter gets rid of the empty strings split creates 
+        # when there are two or more delimiters in a row
         self._risutoset = set(filter(None,newrisuto))
     
     @risutoset.setter
     def risutoset(self,value):
-        raise AttributeError('The item set must be initiated by creating an item list.')
+        raise AttributeError('The item set must' +\
+                                'be initiated by creating an item list.')
     
     @risutoset.deleter
     def risutoset(self):
@@ -70,7 +73,7 @@ class Risuto():
     
     @name.setter
     def name(self,value):
-        self._strvalidation(value,80) # DEFAULT LENGTH FOR NOW
+        self._strvalidation(value,field='name',maxlen=80) # TODO: MAXLEN
         self._name = value
         
     @name.deleter
@@ -83,13 +86,27 @@ class Risuto():
     
     @description.setter
     def description(self,value):
-        self._strvalidation(value,280) # DEFAULT LENGTH FOR NOW
+        self._strvalidation(value,field='description',maxlen=280) # TODO: MAXLEN
         self._description = value
         
     @description.deleter
     def description(self,value):
         del self._description
 
+    @classmethod
+    def fromdict(cls,d):
+        instance = cls()
+        for field in ['text','name','description']:
+            if field in d:
+                setattr(instance,field,d[field])
+        return instance
+
+    def todict(self):
+        d = {'text':self._risutotext,
+             'name':self._name,
+             'description':self._description
+            }
+        return d
 
 class ComparisonForm(FlaskForm):
     risuto1 = SelectField('Left List')
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template, session, redirect, url_for
 from app import app
 from app.Classes import ComparisonForm, RisutoForm, Risuto
+import sqlite3
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    try:
-        text = session['text']
-    except:
-        text = 'Default'
+    if 'risutos' in session:
+        risutos = [x.risutotext for x in session['risutos']]
+    else:
+        risutos = ['Nothing yet']
     lists1 = ['X','Y','Z']
     lists2 = ['B','C','D','E']
     form = ComparisonForm()
@@ -16,17 +17,19 @@ def index():
     form.risuto2.choices = [(u,u) for u in lists2] # Choices must be set after initiation of form
     if form.validate_on_submit():
         pass
-    return render_template('index.html',text=text,form=form)
+    return render_template('index.html',risutos=risutos,form=form)
 
 @app.route('/create',methods=['GET','POST'])
 def create():
     form = RisutoForm()
     if form.validate_on_submit():
-        print('Valid')
-        print(form.risuto.data)
-        session['text'] = form.risuto.data
+        risuto = Risuto()
+        risuto.name = 'Name' + form.risuto.data[:5]
+        risuto.description = 'Desc' + form.risuto.data[:5]
+        risuto.risutotext = form.risuto.data
+        if 'risutos' in session:
+            session['risutos'].append(risuto)
+        else:
+            session['risutos'] = [risuto]
         return redirect(url_for('index'))
-    else:
-        print ('inValid')
-        print(form.risuto.data)
     return render_template('create.html',form=form)
